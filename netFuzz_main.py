6d0273f31d595b8ffff5a283939a5a1211e10af1
vah_13 <vvvaagn@gmail.com>
2016-02-21 12:07:09 +0300

    Update netFuzz_main.py

--- netFuzz_main.py
+++ netFuzz_main.py
@@ -33,8 +33,8 @@ def close_socket(_socket):
 def recieve_data(_socket):
     _socket.recv(1024*1024)
 
-def fuzz_single_package(address, port, raw, loop, seed):
-    _generate_mutated_files(seed, raw, loop)
+def fuzz_dumb_mode(address, port, raw, loop, seed):
+    _generate_mutated_package(seed, raw, loop)
 
     for loop_step in range(loop):
         try:
@@ -53,14 +53,44 @@ def fuzz_single_package(address, port, raw, loop, seed):
             print("exception 2" + ex.message)
     os.system('rm -r /tmp/'+str(loop)+"_1313")
 
-def _generate_mutated_files(seed, raw, loop):
+def _generate_mutated_package(seed, raw, loop):
     os.system('rm -r /tmp/'+str(loop)+"_1313")
     with open('/tmp/vah13_fuzz.bin','wb') as f2:
         f2.write(raw)
     os.system('mkdir /tmp/'+str(loop)+"_1313")
     os.system('cat /tmp/vah13_fuzz.bin | radamsa -n '+str(loop)+' -s '+str(seed)+' -o /tmp/'+str(loop)+"_1313/"+str(seed)+'%n.rdm')
 
-
+def intelectual_fuzz(_address, _port, _package_list, _loop_count, _seed):
+    for __package in _package_list:
+        #start generate package
+        _generate_mutated_package(_seed,__package, _loop_count)
+        #start fuzz loop
+        for __loop_step in range(_loop_count):
+            try:
+                print(_address+":"+str(_port) + " " + str(__loop_step))
+                # get mutade package raw
+                with open('/tmp/'+str(_loop_count)+"_1313/"+str(_seed)+str(__loop_step)+'.rdm', 'r') as file:
+                    raw = file.read().replace('\n', '')
+                try:
+                    with timeout(1, exception=RuntimeException):
+                        __socket = connect_with_socket(_address, _port)
+                        for __package_ in _package_list:
+                            if _package_list.index(__package_) == _package_list.index(__package): 
+                                #send mutate package
+                                send_data(__socket, raw)
+                                recieve_data(__socket)
+                            else:
+                                #send work package
+                                send_data(__socket, __package_)
+                                recieve_data(__socket)
+                        
+                        close_socket(__socket)
+                            
+                except RuntimeException, ex:
+                    print("exception 1" + ex.message)
+            except Exception, ex:
+                print("exception 2" + ex.message)
+            
 def main(argv):
     dump_file = ''
     loop_count = 50000
@@ -106,8 +136,10 @@ def main(argv):
 
     package_list = load_packages_row(dump_file)
     if mode == 0:
-        for _single_package_ in package_list:
-            fuzz_single_package(address,port,_single_package_.replace('\n',''),loop_count, seed)
-
+            fuzz_dumb_mode(address,port,package_list[0].replace('\n',''),loop_count, seed)
+    
+    if mode == 1:
+            intelectual_fuzz(address, port, package_list, loop_count, seed)
+    
 if __name__ == "__main__":
     main(sys.argv[1:])
